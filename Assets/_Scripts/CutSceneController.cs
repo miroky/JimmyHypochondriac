1467e0ab22f3410b1a0d3af3649774056f9b0693
Borja <borjafac90@gmail.com>
2017-11-19 18:21:40 +0100

    FIN DE VERDAD

--- Assets/_Scripts/CutSceneController.cs
+++ Assets/_Scripts/CutSceneController.cs
@@ -6,11 +6,21 @@ using UnityEngine.SceneManagement;
 public class CutSceneController : MonoBehaviour {
 
     public GameObject canvasEnd;
+    public AudioSource endAudio;
+
+    private AudioSource[] allAudioSources;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag.Equals("Player"))
         {
+            allAudioSources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+            foreach (AudioSource audioS in allAudioSources)
+            {
+                if (audioS.isPlaying)
+                    audioS.Stop();
+            }
+            endAudio.Play();
             canvasEnd.SetActive(true);
             Time.timeScale = 0f;
         }
--- Assets/_Scripts/SceneController.cs
+++ Assets/_Scripts/SceneController.cs
@@ -12,18 +12,22 @@ public class SceneController : MonoBehaviour {
 
     public void LoadMainMenu()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("Main_menu");
     }
     public void LoadOpening()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("Opening");
     }
     public void LoadStreet()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("Street");
     }
     public void LoadCredits()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("Credits");
     }
 

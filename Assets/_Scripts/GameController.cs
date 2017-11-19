7ffdf184fbd67c867d7b7842580d9c3df22499ed
vmonty <v.montero2121@gmail.com>
2017-11-19 11:39:56 +0100

    Game over: music Tutorial2: update

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -11,6 +11,8 @@ public class GameController : MonoBehaviour
     public float _secondsDelayGO;
 
     private YuyuController _yuyuController;
+    private AudioSource[] allAudioSources;
+    private bool isGameOver = false;
 
     private void Start()
     {
@@ -18,15 +20,25 @@ public class GameController : MonoBehaviour
     }
     private void Update()
     {
-        CheckGameOver();
+        if(!isGameOver)            
+            CheckGameOver();
     }
 
     private void CheckGameOver()
     {
+        
         // Comprobar si el Yuyu ha llegado a 100
         if (_yuyuController._actualYuyu == 100)
         {
+            isGameOver = true;
+            allAudioSources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+            foreach(AudioSource audioS in allAudioSources)
+            {
+                if (audioS.isPlaying)
+                    audioS.Stop();
+            }
             //Mostrar escena de game over
+            gameOver.GetComponent<AudioSource>().Play();
             gameOver.SetActive(true);
             StartCoroutine(GameOver());
         }
@@ -34,10 +46,13 @@ public class GameController : MonoBehaviour
 
     private IEnumerator GameOver()
     {
+        // Desactivar el control del player
         GameObject.FindGameObjectWithTag("Player").GetComponent<ScrollController>().enabled = false;
         GameObject.FindGameObjectWithTag("Player").GetComponent<ShootingController>().enabled = false;
         GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>().enabled = false;
         GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+
+        // 
         yield return new WaitForSeconds(_secondsDelayGO);
         SceneManager.LoadScene("Street");
     }
